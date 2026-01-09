85b60956d6ab799e7feee422ba02b61602110103
LichtPUBLIC <daffaalyandra65@gmail.com>
2026-01-09 16:37:00 +0700

    Menambahkan navigasi untuk halaman edit siswa

--- app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
@@ -74,6 +74,22 @@ fun HostNavigasi(
             )
         }
 
-
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiEdit.itemIdArg) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            EditSiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                },
+                onNavigateUp = {
+                    navController.navigateUp()
+                }
+            )
+        }
     }
 }
